70391084d306864b439157da3c0aacf6bfb92318
cyanChill <anthonyliang9@gmail.com>
2022-01-13 18:59:40 -0500

    additional levels

--- src/assets/banner-imports.js
+++ src/assets/banner-imports.js
@@ -1,5 +1,9 @@
 import levelOne from "./images/level_1_banner.png";
 import levelTwo from "./images/level_2_banner.png";
+import levelThree from "./images/level_3_banner.png";
+import levelFour from "./images/level_4_banner.png";
+import levelFive from "./images/level_5_banner.png";
+import levelSix from "./images/level_6_banner.png";
 
 const getBanner = (bannerLvlNum) => {
   switch (bannerLvlNum) {
@@ -7,12 +11,20 @@ const getBanner = (bannerLvlNum) => {
       return levelOne;
     case 2:
       return levelTwo;
+    case 3:
+      return levelThree;
+    case 4:
+      return levelFour;
+    case 5:
+      return levelFive;
+    case 6:
+      return levelSix;
     default:
       return null;
   }
 };
 
-const numBanners = 2;
+const numBanners = 6;
 
 export default getBanner;
 export { numBanners };
--- src/assets/character-imports.js
+++ src/assets/character-imports.js
@@ -8,6 +8,26 @@ import mangosteen from "./images/mangosteen.png";
 import pineapple_plant from "./images/pineapple-plant.png";
 import thicc_frog from "./images/thicc-frog.png";
 
+// Level 3
+import toaster from "./images/toaster.png";
+import sad_clown from "./images/sad_clown.png";
+import ghost from "./images/ghost.png";
+
+// Level 4
+import octopus from "./images/octopus.png";
+import toilet from "./images/toilet.png";
+import trident from "./images/trident.png";
+
+// Level 5
+import banana_bunch from "./images/banana_bunch.png";
+import angry_bird from "./images/angry_bird.png";
+import wizard from "./images/wizard.png";
+
+// Level 6
+import igloo from "./images/igloo.png";
+import fishbowl from "./images/fishbowl.png";
+import turtle from "./images/turtle.png";
+
 const getCharImg = (characterName) => {
   switch (characterName) {
     case "bigfoot":
@@ -22,6 +42,30 @@ const getCharImg = (characterName) => {
       return pineapple_plant;
     case "thicc-frog":
       return thicc_frog;
+    case "toaster":
+      return toaster;
+    case "sad-clown":
+      return sad_clown;
+    case "ghost":
+      return ghost;
+    case "octopus":
+      return octopus;
+    case "toilet":
+      return toilet;
+    case "trident":
+      return trident;
+    case "banana-bunch":
+      return banana_bunch;
+    case "angry-bird":
+      return angry_bird;
+    case "wizard":
+      return wizard;
+    case "igloo":
+      return igloo;
+    case "fishbowl":
+      return fishbowl;
+    case "turtle":
+      return turtle;
     default:
       return null;
   }
